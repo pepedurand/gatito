f081abb2c5e37c4476aaa5bc42b3d08313096be7
Pepe <webpedrod@gmail.com>
2022-03-26 16:20:25 -0300

    feat: get fornecedores

--- api/rotas/fornecedores/tabelaFornecedor.js
+++ api/rotas/fornecedores/tabelaFornecedor.js
@@ -6,5 +6,18 @@ module.exports = {
     },
     inserir (fornecedor) {
         return modelo.create(fornecedor)
+    },
+    async pegarPorId (id) {
+        const encontrado = await modelo.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if (!encontrado) {
+            throw new Error('Fornecedor não encontrado')
+        }
+
+        return encontrado
     }
 }
