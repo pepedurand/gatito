dffcb4463918cba3e1ac14759fb5950f774f9911
Pepe <webpedrod@gmail.com>
2022-03-29 21:08:35 -0300

    Final

--- api/index.js
+++ api/index.js
@@ -7,10 +7,25 @@ const NaoEncontrado = require('./erros/NaoEncontrado')
 const CampoInvalido = require('./erros/CampoInvalido')
 const DadosNaoFornecidos = require('./erros/DadosNaoFornecidos')
 const ValorNaoSuportado = require('./erros/ValorNaoSuportado')
-
-
+const formatosAceitos = require('./serializador').formatosAceitos
 
 app.use(bodyParser.json())
+
+app.use((requisicao, resposta, proximo)=> {
+    let formatoRequisitado = requisicao.header('Accept')
+    if(formatoRequisitado==='*/*') {
+        formatoRequisitado = 'application/json';
+    }
+    if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+        resposta.status(406)
+        resposta.end()
+        return
+    }
+
+    resposta.setHeader('Content-Type', formatoRequisitado)
+    proximo()
+})
+
 app.use('/api/fornecedores', roteador)
 
 app.use((erro, requisicao, resposta, proximo) => {
